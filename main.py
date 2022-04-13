372dbca5031dfebfd7296be17fe6c90fc69a9041
Евгений Хромов <hromov.gazprom@yandex.ru>
2022-04-13 22:22:01 +0300

    finalized

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-KEYWORDS = ['TypeScript', 'криминалист', 'помощник', 'IoT']
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux i686; rv:2.0.1) Gecko/20100101 Firefox/4.0.1'
+}
+
+KEYWORDS = ['TypeScript', 'криминалист', 'помощник', 'IoT', 'релиз']
 url = 'https://habr.com/ru/all/'
-response = requests.get(url)
+response = requests.get(url, headers=HEADERS)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 articles = soup.find_all(name='article')
@@ -11,9 +15,12 @@ for article in articles:
     date = article.find_all('time')[0]['datetime'][:10]
     title = article.find_all(class_='tm-article-snippet__title-link')[0].find_all('span')[0].text
     link = 'https://habr.com' + article.find_all(class_='tm-article-snippet__title-link')[0]['href']
+    text = ''
+    list_text = article.find_all('p')
+    for item in list_text:
+        text += item.text
     for keyword in KEYWORDS:
-        if title.find(keyword) != -1:
-            a = title.find(keyword)
+        if title.find(keyword) != -1 or text.find(keyword) != -1:
             print(date + ' - ' + title + ' - ' + link)
             break
 
